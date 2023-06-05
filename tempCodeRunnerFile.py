c24897628bb5c4d37dba6efa97fec0e0d69df2a4
Sankalp Salve <sankalpsalve.en21156180@dypisp.onmicrosoft.com>
2023-06-05 08:35:24 +0530

    all done

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-'''
-
-Q). Consider telephone book database of N clients. Make use of a hash table implementation
-to quickly look up client's telephone number. Make use of two collision handling
-techniques and compare them using number of comparisons required to find a set of
-telephone numbers
-
-'''
